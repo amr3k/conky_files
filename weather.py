64104536ae473cb4c51d581b56994c64610a26a4
Amr <akkk33@protonmail.com>
2020-08-27 02:02:26 +0200

    Added blur (only works with KDE Plasma 5.18+), Added install.sh, Created systemd service to update weather data in background

--- weather.py
+++ weather.py
@@ -1,39 +1,42 @@
-#!/usr/bin/env python3
+from requests import get as requests_get
+from requests import ConnectionError
+from pyowm import OWM
+from shutil import copyfileobj
+from os import environ
+from os.path import expanduser
+from time import sleep
 
+API_KEY = environ.get('OWM_API_KEY')
+CITY_ID = int(environ.get('OWM_CITY_ID'))
+DATA_PATH = f'{expanduser("~")}/.conky/weather'
 
 def get_data(key: str):
     owm = OWM(key)
-    if owm.is_API_online:
-        weather_data = owm.weather_at_id(CITY_ID).get_weather()
-        download_img(f"http://openweathermap.org/img/wn/{weather_data.get_weather_icon_name()}@2x.png")
-        return weather_data
+    try:
+        if owm.is_API_online:
+            weather_data = owm.weather_at_id(CITY_ID).get_weather()
+            download_img(f"http://openweathermap.org/img/wn/{weather_data.get_weather_icon_name()}@2x.png")
+            return weather_data
+    except:
+        return
 
 
 def download_img(icon_url: str):
     try:
-        r = requests_get(icon_url, stream=True)
+        r = requests_get(icon_url, stream=True, timeout=10)
         if r.status_code == 200:
-            with open(f"{DATA_PATH}icon.png", 'wb') as file:
+            with open(f"{DATA_PATH}/icon.png", 'wb') as file:
                 r.raw.decode_content = True
                 copyfileobj(r.raw, file)
     except (Exception):
         return
 
-
-if __name__ == "__main__":
-    from requests import get as requests_get
-    from requests import ConnectionError
-    from pyowm import OWM
-    from shutil import copyfileobj
-    from os import environ
-    from os.path import expanduser
-
-    API_KEY = environ.get('OWM_API_KEY')
-    CITY_ID = int(environ.get('OWM_CITY_ID'))
-    DATA_PATH = f'{expanduser("~")}/.conky/weather/'
-
+def run():
     weather_data = get_data(API_KEY)
-    with open(f"{DATA_PATH}data", 'w') as final_file:
-        final_file.write(f"{weather_data.get_temperature(unit='celsius')['temp']:.0f}\n")
-        final_file.write(f"{weather_data.get_wind()['speed']*3.6:.1f}\n")
-        final_file.write(f"{weather_data.get_humidity()}%")
+    if weather_data:
+        with open(f"{DATA_PATH}/data", 'w') as final_file:
+            final_file.write(f"{weather_data.get_temperature(unit='celsius')['temp']:.0f}\n")
+            final_file.write(f"{weather_data.get_wind()['speed']*3.6:.1f}\n")
+            final_file.write(f"{weather_data.get_humidity()}%")
+if __name__ == "__main__":
+    run()
